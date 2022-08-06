9c4dc7104095ab8f3bddb785ddef0b500a5f29d4
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2022-08-06 16:43:37 +1200

    updates to Marae.kt
    added a toString method

---
+++ app/src/main/java/com/example/maps/Marae.kt
@@ -0,0 +1,93 @@
+package com.example.maps
+
+import com.google.android.gms.maps.model.LatLng
+//import org.apache.commons.csv.CSVFormat
+//import org.apache.commons.csv.CSVParser
+import java.io.BufferedReader
+import java.io.FileReader
+import java.io.InputStreamReader
+import java.nio.channels.AsynchronousFileChannel.open
+import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.MarkerOptions
+
+// TODO ive removed anycode that comes up with redlines, just using it as a mock class for now
+
+class Marae(_name: String, _x: Double, _y: Double, _location: String, _iwi: String) {
+    //implement below as complete constructor
+    // class Marae (_name: String, _wharenui: String, _x: Double, _y: Double, _location: String, _iwi: String, _hapu: String, _search: String) {
+
+    var name: String
+
+    // var wharenui: String
+    var x: Double
+    var y: Double
+    var position: LatLng
+    var location: String
+    var iwi: String
+    // var hapu: String
+    // var search: String
+
+    init {
+        this.name = _name
+        //this.wharenui = _wharenui
+        this.x = _x
+        this.y = _y
+        this.position = LatLng(x, y)
+        this.location = _location
+        this.iwi = _iwi
+        //this.hapu = _hapu
+        //this.search = _search
+
+    }
+
+    override fun toString() : String{
+        TODO("Implement me!")
+        return "A mock marae description"
+    }
+
+    constructor() : this("default", 0.0, 0.0, "default_location", "default_iwi")
+}
+
+// create getMaraeData method
+// needs to be fed Paths.get("/mapdata/Marae.csv"));
+fun getMaraeData(bufferedReader: BufferedReader): Array<Marae> {
+
+    // var csvParser = CSVParser(bufferedReader, CSVFormat.DEFAULT)
+
+    var maraeData = Array(1058) { Marae() }
+    var i = 0
+
+    /*
+    // iterate through CSV file
+    for (csvRecord in csvParser) {
+
+        var name = csvRecord.get(1)
+        var x = csvRecord.get(5).toDouble()
+        var y = csvRecord.get(6).toDouble()
+        var location = csvRecord.get(7)
+        var iwi = csvRecord.get(8)
+
+
+        // update maraeData at i
+        maraeData[i].name = name
+        maraeData[i].x = x
+        maraeData[i].y = y
+        maraeData[i].location = location
+        maraeData[i].iwi = iwi
+        println("-- -- -- -- -- --")
+        println("marae Name : ${maraeData[i].name}")
+        println("-- -- -- -- -- --")
+
+        var i = i + 1
+
+
+    }
+
+     */
+    return maraeData
+
+}
+
+
+
