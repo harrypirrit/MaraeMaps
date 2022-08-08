5e62a1179a84c7066328cfed6dfdbfac1a2f54ad
chaka160 <kchay@windowslive.com>
2022-08-08 15:42:00 +1200

    Created the activity to display all the information required initially.
    TO DO:
    
    - Settings activity needs linked to the About activity.
    - Set up fragments so that About Us and credits sections have a window pop up as a result of being clicked.
    - Input data in fragments for About us sections

--- app/src/main/java/com/example/maps/About.kt
+++ app/src/main/java/com/example/maps/About.kt
@@ -8,34 +8,26 @@ package com.example.maps
  */
 
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ListView
 import android.widget.ArrayAdapter
 
 class About : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
 
-        val ourList = arrayOf("Settings","About us","Credits")
         val ourListView = findViewById<ListView>(R.id.screenListView)
+        val ourList = arrayOf("Settings","About us","Credits")
+
         val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, ourList)
         ourListView.adapter = adapter
 
         ourListView.setOnItemClickListener{parent, view, position, id ->
-
-            // Upon click, this will go to Settings activity.
-            if (position==0){
-
-            }
-            // Upon click, this will go to About Us fragment.
-            if (position==1){
-
-            }
-            // Upon click, this will go to Credits fragment.
-            if (position==2){
-
-            }
 
         }
 
