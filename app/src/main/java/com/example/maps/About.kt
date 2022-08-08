6da2f37236f7bb497ad4b60d5f51f26036304eef
chaka160 <kchay@windowslive.com>
2022-08-08 15:00:02 +1200

    Created the activity to display all the information required initially.
    TO DO:
    
    - Settings activity needs linked to the About activity.
    - Set up fragments so that About Us and credits sections have a window pop up as a result of being clicked.
    - Input data in fragments for About us sections

--- app/src/main/java/com/example/maps/About.kt
+++ app/src/main/java/com/example/maps/About.kt
@@ -1,18 +1,34 @@
 package com.example.maps
 
 /**
- * Activity for containing the about us, settings and other fragments.
- *
+ * Activity for containing the about us and credits fragments.
+ * Also has link to Settings activity.
  * @author Kavan Chay
  *
  */
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import android.widget.ArrayAdapter
+
 
 class About : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        val ourList = arrayOf("Settings","About us","Credits")
+        val ourListView = findViewById<ListView>(R.id.screenListView)
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, ourList)
+        ourListView.adapter = adapter
+
+        ourListView.setOnItemClickListener{parent, view, position, id ->
+
+            /* what happens at positions one, two, three */
+            //Need an onclick and intent to set to SettingsActivity
+
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/maps/SettingsActivity.kt
@@ -0,0 +1,31 @@
+package com.example.maps
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
+
+/**
+ * Settings activity for IwiMaps app.
+ * @author Kavan Chay
+ */
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+    }
+}
