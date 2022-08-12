c550372d21a81889efc4044ca2e5924d99639028
Harry Pirrit <harrypirrit@Harrys-MBP-2.student.uod.otago.ac.nz>
2022-08-12 13:23:43 +1200

    added changes to MapFragment

--- app/src/main/java/com/example/maps/MapsFragment.kt
+++ app/src/main/java/com/example/maps/MapsFragment.kt
@@ -7,12 +7,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.maps.R
+import com.example.maps.core.Marae
+import com.example.maps.core.MaraeController
+import com.google.android.gms.common.api.GoogleApi
+import com.google.android.gms.maps.*
 
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsFragment : Fragment() {
@@ -27,9 +28,31 @@ class MapsFragment : Fragment() {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
+        googleMap.mapType = googleMap
+
+//            GoogleMapOptions()
+//                .mapId(resources.getString(R.string.map_id))
+
+
+
         val sydney = LatLng(-34.0, 151.0)
         googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+//        val maraeData = Array(10) { Marae() }
+//        maraeData[1] = Marae("Marae 1", -45.83955732551009, 175.4870606057339, "Test Location", "Test Iwi")
+//        maraeData[2] = Marae("Ahikiwi", -45.83955732551009, 180.4870606057339, "Test Location", "Test Iwi")
+//        maraeData[3] = Marae("Akerama", -45.83955732551009, 180.4870606057339, "Test Location", "Test Iwi")
+
+        // Create a Marker array and iterate through marae to add them to the map
+        var mMarkers: ArrayList<Marker> = ArrayList()
+
+//        for (marae in MaraeController.maraeCollection) {
+//            val LL = LatLng(marae.x, marae.y)
+//            mMarkers.add(
+//                googleMap.addMarker(MarkerOptions().position(LL).title(marae.name))
+//            )
+//        }
     }
 
     override fun onCreateView(
