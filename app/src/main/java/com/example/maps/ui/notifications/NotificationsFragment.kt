158d955709ec02bd935728555569081bfccd32ce
chaka160 <kchay@windowslive.com>
2022-08-14 15:38:24 +1200

    - Home button now has contents of Map fragment. - Wiki button now gets to a search function, which is clickable. - Info button now clicks to a list, which needs to be updated to have navigation to the other fragments
    TODO:
    - Settings still needs implemented (what settings?)
    - Understand the View Model implementation, how can we use this?

--- app/src/main/java/com/example/maps/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/maps/ui/notifications/NotificationsFragment.kt
@@ -7,7 +7,14 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.maps.InfoFragment
+import com.example.maps.R
 import com.example.maps.databinding.FragmentNotificationsBinding
+import com.example.maps.ui.MyInfoRecyclerViewAdapter
+import com.example.maps.ui.placeholder.PlaceholderContent
 
 class NotificationsFragment : Fragment() {
 
@@ -17,20 +24,52 @@ class NotificationsFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private var columnCount = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            columnCount = it.getInt(ARG_COLUMN_COUNT)
+        }
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_info_list, container, false)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        return root
+        // Set the adapter
+        if (view is RecyclerView) {
+            with(view) {
+                layoutManager = when {
+                    columnCount <= 1 -> LinearLayoutManager(context)
+                    else -> GridLayoutManager(context, columnCount)
+                }
+                adapter = MyInfoRecyclerViewAdapter(PlaceholderContent.ITEMS)
+            }
+        }
+        return view
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+
+        // TODO: Customize parameter argument names
+        const val ARG_COLUMN_COUNT = "column-count"
+
+        // TODO: Customize parameter initialization
+        @JvmStatic
+        fun newInstance(columnCount: Int) =
+            InfoFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(ARG_COLUMN_COUNT, columnCount)
+                }
+            }
     }
 }
